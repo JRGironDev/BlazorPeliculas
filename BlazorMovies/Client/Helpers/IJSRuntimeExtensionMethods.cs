30db973b3a29c6f3f7ade9b251fa4ad11f436988
Jose Quiñónez <joserobertoq@outlook.com>
2023-08-10 07:41:54 -0600

    Se agregó el método JSRuntime

---
+++ BlazorMovies/Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using BlazorMovies.Shared.Entidades;
+using Microsoft.JSInterop;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string mensaje)
+        {
+            await js.InvokeVoidAsync("console.log", "prueba de console log");
+            return await js.InvokeAsync<bool>("confirm", mensaje);
+        }
+    }
+}
