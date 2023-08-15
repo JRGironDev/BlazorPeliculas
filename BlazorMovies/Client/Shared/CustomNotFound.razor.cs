2554ed56727304be6b63afd4b40bea5447c73fa3
Jose Quiñónez <joserobertoq@outlook.com>
2023-08-15 15:23:22 -0600

    Se agregaron componentes de creación y edición

---
+++ BlazorMovies/Client/Shared/CustomNotFound.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BlazorMovies.Client.Shared
+{
+    public partial class CustomNotFound
+    {
+        public CustomNotFound()
+        {
+
+        }
+    }
+}
