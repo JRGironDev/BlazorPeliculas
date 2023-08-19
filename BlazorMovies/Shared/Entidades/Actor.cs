cc05337f7db21797a1e17cd6cf4beb08ce0ca95c
Jose Quiñónez <joserobertoq@outlook.com>
2023-08-19 08:48:02 -0600

    Se agregó el SelectorMultiple para Actores

--- BlazorMovies/Shared/Entidades/Actor.cs
+++ BlazorMovies/Shared/Entidades/Actor.cs
@@ -10,14 +10,24 @@ namespace BlazorMovies.Shared.Entidades
     public class Actor
     {
         public int Id { get; set; }
-
         [Required]
         public string Nombre { get; set; } = null!;
-
         public string? Biografia { get; set; }
-
         public string? Foto { get; set; }
-
         public DateTime? FechaNacimiento { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Actor actor)
+            {
+                return Id == actor.Id;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
